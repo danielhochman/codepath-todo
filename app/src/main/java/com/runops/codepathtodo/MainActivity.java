841c82bb297e8d46be310b65dc07a4c240e9de2d
Daniel Hochman <dhochman@lyft.com>
2015-02-05 19:28:59 -0800

    create items list and wire up to view with adapter

--- app/src/main/java/com/runops/codepathtodo/MainActivity.java
+++ app/src/main/java/com/runops/codepathtodo/MainActivity.java
@@ -4,14 +4,28 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView listViewItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        listViewItems = (ListView) findViewById(R.id.listViewItems);
+        listViewItems.setAdapter(itemsAdapter);
     }
 
 

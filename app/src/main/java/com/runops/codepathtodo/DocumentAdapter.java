bae732d9fa37129cb9c9927f19c5a7e31a7d7aed
Daniel Hochman <danielhochman@runops.com>
2015-02-10 01:56:29 -0800

    wip of due date

--- app/src/main/java/com/runops/codepathtodo/DocumentAdapter.java
+++ app/src/main/java/com/runops/codepathtodo/DocumentAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.couchbase.lite.Document;
@@ -41,6 +42,7 @@ public class DocumentAdapter extends ArrayAdapter<Document> {
             holder = new DocumentHolder();
             holder.title = (TextView) row.findViewById(R.id.itemTitle);
             holder.checkbox = (CheckBox) row.findViewById(R.id.itemCheckbox);
+            holder.dueAt = (TextView) row.findViewById(R.id.itemDueAt);
 
             row.setTag(holder);
         } else {
@@ -51,11 +53,18 @@ public class DocumentAdapter extends ArrayAdapter<Document> {
         holder.title.setText((String) document.getProperty("itemText"));
         holder.checkbox.setChecked((boolean) document.getProperty("itemChecked"));
 
+        if (document.getProperty("itemDueAt") == null) {
+            holder.dueAt.setText("");
+        } else {
+            holder.dueAt.setText("DUE " + "");
+        }
+
         return row;
     }
 
     static class DocumentHolder {
         TextView title;
         CheckBox checkbox;
+        TextView dueAt;
     }
 }
--- app/src/main/java/com/runops/codepathtodo/EditItemActivity.java
+++ app/src/main/java/com/runops/codepathtodo/EditItemActivity.java
@@ -3,10 +3,11 @@ package com.runops.codepathtodo;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class EditItemActivity extends ActionBarActivity {
@@ -15,6 +16,7 @@ public class EditItemActivity extends ActionBarActivity {
     Integer itemPosition;
 
     EditText editTextExistingView;
+    TextView textViewDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +24,14 @@ public class EditItemActivity extends ActionBarActivity {
         setContentView(R.layout.activity_edit_item);
 
         editTextExistingView = (EditText) findViewById(R.id.editTextExistingItemView);
+        textViewDate = (TextView) findViewById(R.id.textViewDate);
 
         itemText = getIntent().getStringExtra("itemText");
         itemPosition = getIntent().getIntExtra("itemPosition", -1);
 
         editTextExistingView.setText(itemText);
+//        textViewDate.setText("Tue, May 3, 2014" + "     ");
+
     }
 
     public void onSaveItem(View view) {
@@ -35,5 +40,8 @@ public class EditItemActivity extends ActionBarActivity {
         intent.putExtra("itemPosition", itemPosition);
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    public void onTextViewDateClick(View view) {
     }
 }

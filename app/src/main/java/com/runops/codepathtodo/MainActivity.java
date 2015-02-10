623f5a3446d82f3b5c1a16b11f62fa39794a631d
Daniel Hochman <danielhochman@runops.com>
2015-02-09 23:16:23 -0800

    no need to save item position to database

--- app/src/main/java/com/runops/codepathtodo/MainActivity.java
+++ app/src/main/java/com/runops/codepathtodo/MainActivity.java
@@ -130,7 +130,6 @@ public class MainActivity extends ActionBarActivity {
         // Save the item to the database
         Map<String, Object> docContent = new HashMap<String, Object>();
         docContent.put("itemText", itemText);
-        docContent.put("itemPosition", items.size() - 1);
         docContent.put("itemCreatedAt", nowAsString);
         Document document = db.createDocument();
         try {
